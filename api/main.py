c734816f92cdea86db4b59fbb06389a654ebc8d4
Mireu Lim <limmireu1214@gmail.com>
2024-09-27 22:14:44 +0900

    feat: upload main.py and dockerfile.package upload

--- api/main.py
+++ api/main.py
@@ -1,9 +1,15 @@
-from flask import Flask
-app = Flask(__name__)
+from typing import Union
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+from fastapi import FastAPI
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=)
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
