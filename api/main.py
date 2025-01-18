fe8ee2d3d2d7495e74cb71d79ce91751a0b19a1a
mireu-lab <limmireu1214@gmail.com>
2025-01-18 07:34:10 +0000

    update main.py

--- api/main.py
+++ api/main.py
@@ -1,5 +1,4 @@
 from typing import Union
-
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -9,11 +8,10 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", port=5000, log_level="info")
+    uvicorn.run("main:app", host="0.0.0.0", port=5000, log_level="info")
